bb03643ed27bfec82c4792d5a63f28dbf060763b
Denizard Dimitri Camargo <denizarddimitricamargo@gmail.com>
2022-01-08 15:47:03 -0400

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthdate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthdate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Birthdate = birthdate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sale)
+        {
+            Sales.Add(sale);
+        }
+
+        public void RemoveSals(SalesRecord sale)
+        {
+            Sales.Remove(sale);
+        }
+
+        public double TotalSales(DateTime inicialDate, DateTime finalDate)
+        {
+            return Sales.Where(x => x.Date >= inicialDate && x.Date <= finalDate) //filtra pela data
+                .Sum(x => x.Amount); //faz o somatório da lista já filtrada
+        }
+    }
+}
