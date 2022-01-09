b70983a8556b4208131c753776788c6ffc642b5c
Denizard Dimitri Camargo <denizarddimitricamargo@gmail.com>
2022-01-09 00:17:34 -0400

    Seeding Service

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -8,7 +8,7 @@ namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
@@ -18,9 +18,8 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        public SalesRecord(DateTime date, double amount, SaleStatus status, Seller seller)
         {
-            this.id = id;
             Date = date;
             Amount = amount;
             Status = status;
